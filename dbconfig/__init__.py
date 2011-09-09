d27a924b00d896ccfca5586c7428a762033aaa3c
Viktor Kotseruba <barbuzaster@gmail.com>
2011-09-09 04:27:36 +0400

    explicit exports

--- dbconfig/__init__.py
+++ dbconfig/__init__.py
@@ -4,4 +4,9 @@ from models import ConfigFile
 from utils import ConfigGroup, autodiscover
 from context import config_context
 
+
+__all__ = ("VERSION", "ConfigFile", "ConfigGroup",
+           "autodiscover", "config_context", )
+
+
 VERSION = (0, 1, 7)
--- dbconfig/context.py
+++ dbconfig/context.py
@@ -2,6 +2,10 @@
 
 import utils
 
+
+__all__ = ("config_context", )
+
+
 def config_context(request):
     return dict([(group._meta.name, group)
                  for group in utils.registry])
